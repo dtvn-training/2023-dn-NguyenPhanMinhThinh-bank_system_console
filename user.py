fd20b01bf92df0b5aca44ff6eab397b917d55883
Zan <minhthinh130202@gmail.com>
2023-11-02 13:04:43 +0700

    #1 Create Class User

---
+++ user.py
@@ -0,0 +1,10 @@
+class User():
+    def __init__(self, citizen_id, full_name, date_of_birth, gender, address, phone_number, email):
+        self.citizen_id = citizen_id
+        self.full_name = full_name
+        self.date_of_birth = date_of_birth
+        self.gender = gender
+        self.address = address
+        self.phone_number = phone_number
+        self.email = email
+
